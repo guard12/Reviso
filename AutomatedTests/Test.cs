a6d8e7f0302f4d1b235bb2f8a462b62c3fc2058f
Matus <matuskollarcik@MacBook-Air-2.local>
2018-03-30 01:05:16 +0200

    Automated testing implementation
    implemented example of automated testing

--- AutomatedTests/Test.cs
+++ AutomatedTests/Test.cs
@@ -3,19 +3,40 @@ using System;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+
 namespace AutomatedTests
 {
-    [TestFixture()]
+    [TestFixture]
     public class Test
     {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void Init()
+        {
+            ChromeDriverService service = ChromeDriverService.CreateDefaultService(@"/Users/matuskollarcik/Desktop/Reviso/Reviso/Drivers", "chromedriver");
+            service.Port = 64444;
+            driver = new ChromeDriver(service);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            //driver.Url = "http://localhost:5000/";
+        }
 
         [Test]
         public void TestCase()
         {
-            IWebDriver driver = new ChromeDriver();
-            driver.Navigate().GoToUrl("http://localhost:5000/");
-            driver.Close();
-            //driver.FindElement();
+            
+            driver.Navigate().GoToUrl("https://www.google.com");
+            IWebElement input = driver.FindElement(By.Id("lst-ib"));
+            input.SendKeys("Reviso bookkeeping");
+            input.SendKeys(Keys.Enter);
+            IWebElement firstResult = driver.FindElement(By.ClassName("bkWMgd"));
+            firstResult.Click();
+
+        }
+
+        [TearDown]
+        public void CleanUp()
+        {
         }
     }
 }
