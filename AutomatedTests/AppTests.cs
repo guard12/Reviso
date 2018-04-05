6b7ee634f7da25a7694c1dbd570ab5cd44ef9ff4
Matus <matuskollarcik@MacBook-Air-2.local>
2018-04-05 09:04:47 +0200

    Added ApTests file
    new file with nunit test of GetWorkingTimes() method in DBGetWorkingTime

---
+++ AutomatedTests/AppTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using NUnit.Framework;
+using Reviso.DAL;
+using Reviso.Model;
+using System.Collections.Generic;
+
+namespace Reviso.AutomatedTests
+{
+    [TestFixture]
+    public class AppTests
+    {
+        [TestCase]
+        public void GetWorkingTimes()
+        {
+            List<TimeLog> times = new List<TimeLog>();
+            DBGetWorkingTime dbgwt = new DBGetWorkingTime();
+            times = dbgwt.GetAllWorkingTimes();
+            Assert.IsTrue(times.Count > 0);
+        }
+    }
+}
